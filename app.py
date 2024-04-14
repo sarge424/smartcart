e01ee3fc047935d4977be0a86fca40bfdd492cd8
Alex Dawson <arjunsivakumar2@gmail.com>
2024-04-14 22:57:51 +0530

    added item lists

--- app.py
+++ app.py
@@ -11,42 +11,57 @@ templates = Jinja2Templates(directory='htmls')
 
 sessions = {}
 
+carts = {}
+
+#LOGIN PAGE
 @app.get('/')
 async def login_page(request: Request):
     return templates.TemplateResponse('login.html', {'request': request})
 
+
+#LOGIN TO CART
 @app.post('/')
 async def check_cartid(cartno: Annotated[str, Form()]):
     if cartno in sessions.keys():
         return RedirectResponse('/', status_code=status.HTTP_302_FOUND)
     
+    #init empty cart and send session cookie
+    carts[cartno] = ['a', 'b', 'c']
     sessions[cartno] = str(hash(cartno + '50792bn-3hn' + datetime.now().strftime('%H%M%S')))
     response = RedirectResponse(f'/cart/{cartno}', status_code=status.HTTP_302_FOUND)
     response.set_cookie(key="session", value = sessions[cartno])
     return response
 
-@app.get('/cookie')
-def show_cookies(request: Request):
-    return {'cookies': request.cookies}
 
+#CART PAGE
 @app.get('/cart/{cartno}')
 async def show_cart(request: Request, response: Response, cartno: str):
+    #session mismatch - send to homepage
     if 'session' not in request.cookies.keys() or cartno not in sessions.keys() or sessions[cartno] != request.cookies['session']:
         response.delete_cookie('session')
         return RedirectResponse('/', status_code=status.HTTP_302_FOUND)
     
+    #TODO: DUMMY PAGE
     return templates.TemplateResponse('cart.html', {
         'request': request,
         'cartno': cartno,
-        'session_id': request.cookies['session']
+        'items': carts[cartno]
     })
     
+#LOGOUT
 @app.get('/logout')
 async def logout(request: Request):
-    global sessions
+    global sessions, carts
+    
+    #clear the session and cart corresponding to this cartno
     response = RedirectResponse('/', status_code=status.HTTP_302_FOUND)
-    if 'session' in request.cookies.keys():
-        sessions = {k:v for k,v in sessions.items() if v != request.cookies['session']}
+    for k in sessions.keys():
+        if sessions[k] == request.cookies['session']:
+            print('cleared session', k, sessions[k], carts[k])
+            del carts[k]
+            del sessions[k]
+            break
+        
     response.delete_cookie('session')
     return response
     
